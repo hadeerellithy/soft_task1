5af0fb0f921f072dc55101414cf506aad681ea1c
Hadeer M Ellithy <112807091+hadeerellithy@users.noreply.github.com>
2023-04-20 11:54:56 +0200

    soft_task1

---
+++ database/studentsArray.js
@@ -0,0 +1,47 @@
+export const students = [
+    {
+        id: 1,
+        name: "Habiba",
+        city: "Cairo",
+    },
+    {
+        id: 2,
+        name: "Mohammed",
+        city: "Shibeen",
+    },
+    {
+        id: 3,
+        name: "Ahmed",
+        city: "Cairo",
+    },
+    {
+        id: 4,
+        name: "Nour",
+        city: "Giza",
+    },
+    {
+        id: 5,
+        name: "Samar",
+        city: "Giza",
+    },
+    {
+        id: 6,
+        name: "Mai",
+        city: "Giza",
+    },
+    {
+        id: 7,
+        name: "Nader",
+        city: "Tanta",
+    },
+    {
+        id: 8,
+        name: "Hossam",
+        city: "Alex",
+    },
+    {
+        id: 9,
+        name: "Hesham",
+        city: "Cairo",
+    }
+];
